edb0a56234f4db9d354c4500ab5c4bd9eb384cee
SBViside <ohvatuki@gmail.com>
2023-04-13 12:19:13 +0300

    upload

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
-import App from './App';
-import './other/i18n';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
