07bc475446e20e172adfc96e5bc028c5b8f21661
SBViside <ohvatuki@gmail.com>
2023-06-27 17:45:21 +0300

    ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import LanguageSwitcher from '../language-switcher/LanguageSwitcher';
 import ThemeSwitcher from '../theme-switcher/ThemeSwitcher';
 import classes from './header.module.scss';
+import { themeContext } from '../../other/themeContext';
 
 export default function Header() {
+	const { isDark } = useContext(themeContext);
+
 	return (
-		<header className={classes.header}>
-			<div className={[classes.headerContent, 'container'].join(' ')}>
+		<header className={isDark ? classes.header_dark : classes.header_light}>
+			<div className={[classes.content, 'container'].join(' ')}>
 				<LanguageSwitcher />
 				<ThemeSwitcher />
 			</div>
