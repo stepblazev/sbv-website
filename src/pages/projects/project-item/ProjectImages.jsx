d53cfaadbd33336998fa17426f53da53cfeca662
SBViside <ohvatuki@gmail.com>
2023-07-02 12:02:59 +0300

    1.1.0 Swipeable images

--- src/pages/projects/project-item/ProjectImages.jsx
+++ src/pages/projects/project-item/ProjectImages.jsx
@@ -2,20 +2,32 @@ import { useState } from 'react';
 import ImageLoader from '../../../components/_UI/image-loader/ImageLoader';
 import { MdOutlineNavigateNext } from 'react-icons/md';
 import classes from './project-item.module.scss';
+import { useSwipeable } from 'react-swipeable';
 
 export default function ProjectImages({ images }) {
 	const [slide, setSlide] = useState(0);
+
+	const swipeHandlers = useSwipeable({
+		onSwipedLeft: () => {
+			if (slide != images.length - 1) setSlide((prev) => prev + 1);
+		},
+		onSwipedRight: () => {
+			if (slide != 0) setSlide((prev) => prev - 1);
+		},
+		preventScrollOnSwipe: true,
+	});
 
 	return (
 		<div className={classes.images}>
 			<div
 				className={classes.images__container}
 				style={{ width: `${images.length * 100}%`, marginLeft: `-${100 * slide}%` }}
+				{...swipeHandlers}
 			>
 				{images.map((src) => (
-					<a key={src} target='_blank' href={src} className={classes.image}>
+					<div key={src} target='_blank' href={src} className={classes.image}>
 						<ImageLoader src={src} />
-					</a>
+					</div>
 				))}
 			</div>
 			{images.length > 1 && (
